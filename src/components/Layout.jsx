360cbac9ef9fe1b908964e92c0c606a96128c029
Himanshu-swn <swn.himanshu@gmail.com>
2025-01-19 00:18:20 +0530

    added scrolling bar, favicon and other minor changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollingBar from "./ScrollingBar";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      <ScrollingBar />
       <Header />
       <main className="flex-grow">{children}</main>
       {/* {console.log(children)} */}
